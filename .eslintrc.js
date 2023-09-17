a53f3d4e254f457775d8d7487543b038537ddcf9
Jashon Osala <jashonosala@gmail.com>
2023-09-17 23:53:34 +0300

    Update eslintrc file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
       'no-underscore-dangle': 'off',
       'no-console': 'off',
       'no-shadow': 'off',
+      'import/no-named-as-default': 'off',
+      'no-unused-vars': 'off',
       'no-restricted-syntax': [
         'error',
         'LabeledStatement',
